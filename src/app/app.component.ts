feac66f29ccd1fd581938fbcb4b61283b5dcefbd
Ivan Oliveira <ivanvoliveira@gmail.com>
2022-08-01 18:38:03 -0300

    implementação de contador de segundos até 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-project';
+export class AppComponent implements OnInit{
+  count: number = 0;
+  
+  constructor () {
+
+  }
+
+  ngOnInit() {
+    let interval: number = setInterval(() => {
+      this.count++;
+      if(this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
 }
